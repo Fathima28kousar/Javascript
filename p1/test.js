67b6e0d05ce6cafae31cb4c31d51a43d0f32c139
Fathima28kousar <kousar28fathima@gmail.com>
2023-11-22 17:32:45 +0530

    S

---
+++ p1/test.js
@@ -0,0 +1,35 @@
+//document.write("Hello world <br> <i>hmmmm!!!</i>");
+// var firstname55 = 'hello fathima';
+// var a = 5674;
+// document.write(firstname55)
+// document.write(a)
+
+/*var per = 45;
+if (per>=80 && per <=100){
+ console.log("Your in merit");
+}else if (per>60 && per<80){
+ console.log("Your in Ist division");
+}else if (per>35 && per <60){
+ console.log("Your in 2nd division");
+ }else if (per<35){
+ console.log("Your Failed");
+} else {
+ console.log("Please enter valid percentage");
+}*/
+
+/*var name = 'Yahoo Baba';
+var gender = 'Male';
+if (gender == 'Male'){
+    console.log("Hello Mr."+name);
+}else{
+    console.log("Hello Ms."+name)
+}*/
+
+
+/*var x = 100;
+if (x>30){
+    console.log('x is greater');
+}else{
+    console.log('x is not greater');
+}*/
+
---
+++ p2/first.js
@@ -0,0 +1,130 @@
+// alert('this is an alert khamoshh!!');
+// console.log('atif aslam');
+// age = 24;
+// price = 99.99;
+// radius = -12;
+// a = null;
+// y = undefined;
+// console.log(age);
+// console.log(price);
+// console.log(radius);
+// console.log(a);
+// console.log(y);
+// age = 24;
+// age = 'fathima';
+// console.log(age);
+// age = 14;
+// Age = 15;
+// console.log(age);
+// console.log(Age);
+// agE_$1 = 'this is an age';
+// console.log(agE_$1 )
+// Console = 'fathima';
+// console.log(Console);
+
+// var age = 23;
+// var age = 24;
+// var age = 25;
+// console.log(age);
+
+// let age = 23;
+// age = 24;
+// age = 25;
+// console.log(age);
+// let a;
+// console.log(a);
+
+// let b;
+// b = 23;
+// console.log(b);
+
+// const radius = 45;
+// console.log(radius);
+// {
+//     let a = 5;
+//     console.log(a);
+// }
+
+// {
+//     let a = 10;
+//     console.log(a)
+// }
+
+// let x;
+// console.log(x)
+// typeof x;
+// console.log(typeof x)
+const student = {
+    fullName : 'fathima',
+    age : 20,
+    cgpa : 22,
+    isPass : true,
+
+};
+
+// console.log(student.age)
+// console.log(student['age'])
+
+// const x = 5;
+// const y = 100;
+// let z = x+y;
+// console.log(z)
+    // const pi = 3.14;
+    // let person = 'fathima';
+    // let answer = 'Yes correct answer';
+    // console.log(pi);
+    // console.log(person);
+    // console.log(answer);
+
+
+/*const product = {
+    productName : 'Parker Pen',
+    rating : 5,
+    isDeal : true,
+    price : 270,
+};
+
+console.log(product);
+console.log(typeof product);*/
+// const profile = {
+//     fullName : 'fathima',
+//     isFollow : false,
+//     message : 'hello how are you',
+//     posts : 195,
+//     followers : 569000
+// };
+// console.log(profile);
+// console.log(typeof(profile['fullName']));
+
+// let a = 10;
+// let b = 29;
+
+// console.log('a =',a,'b =',b);
+// a = a+1;
+// console.log('++a = ',++a);
+// // console.log('a++ = ',a++);
+
+// let age = 12;
+
+// console.log(age >10 ? "yes" : "no")
+
+// let number = prompt("Enter the number!");
+// if (number %5 ===0){
+//     console.log('this is divisible by 5')   
+// }else{
+//     console.log('this is not divisible by 5')
+// }
+
+let num = prompt('Enter the number!')
+
+if (num <=100 && num >=90){
+    grade = 'A';
+} else if (num <=89 && num >=70){
+    grade = 'B';
+} else if (num <= 69 && num >=60){
+    grade = 'C';
+}else if (num <= 59 && num >=50){
+    grade = 'D';
+}else grade = 'F';
+
+console.log("according to your scores, your grade was: ",grade)
