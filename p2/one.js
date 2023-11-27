477894bc47a443f8e7d0a71fd0ac85cf9a8ce6a9
Fathima28kousar <kousar28fathima@gmail.com>
2023-11-27 14:47:32 +0530

    S

--- p2/one.js
+++ p2/one.js
@@ -1,3 +1,34 @@
-var prompt = require('prompt-sync')();
-let a = prompt("Enter your name");
-console.log(a);
+// var prompt = require('prompt-sync')();
+
+// function myFunction(msg){
+//     console.log(msg);
+// }
+
+// myFunction('this is fathima kousar');
+// function multi(a,b){
+//     return a*b;
+// }
+// let res = multi(3,4);
+// console.log(res);
+// function countVowels(st) {
+//   let count = 0;
+//   for (let i of st) {
+//     if (i === "a" || i === "e" || i === "i" || i === "o" || i === "u") {
+//       count = count+1;
+//     }
+//   }
+//   return count;
+// }
+// console.log(countVowels("hello"));
+
+const arrowCount = (sta) => {
+    let count = 0;
+    for (let i of sta) {
+    if (i === "a" || i === "e" || i === "i" || i === "o" || i === "u") {
+      count = count+1;
+    }
+  }
+  return count;
+}
+console.log(arrowCount("hello"));
+
