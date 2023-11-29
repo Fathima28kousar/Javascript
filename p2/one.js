87e9751cbc6d65a369d68240a112a43135c3d750
Fathima28kousar <kousar28fathima@gmail.com>
2023-11-29 17:15:09 +0530

    E

--- p2/one.js
+++ p2/one.js
@@ -1,4 +1,4 @@
-// var prompt = require('prompt-sync')();
+/*// var prompt = require('prompt-sync')();
 
 // function myFunction(msg){
 //     console.log(msg);
@@ -32,4 +32,72 @@
 // }
 // console.log(arrowCount("hello"));
 
-let arr = [1,2,3,4,5]
+// function myFunc(){
+//   console.log('Welcome to Apna college');
+//   console.log('We are learning javascript');
+// }
+// myFunc();
+
+
+// function myFuncton(msg){
+//   console.log(msg);
+// }
+// myFuncton('I like javascript language');
+// myFuncton();
+
+/*function myFunction(msg,n){
+  console.log(msg*n);
+}
+myFunction('this is good',10);*/
+// function sum(x,y){
+//   console.log(x+y);
+// }
+// sum(2,3);
+
+// function sums(x,y){
+//   s = x+y;
+//   return s;
+// }
+// console.log(sums(2,4));
+// /*function countVowels(sta){
+
+ // let count = 0;
+ // for (let i of sta){
+  //  if (i=='a'||i=='e'||i=='i'||i=='o'||i=='u'){
+  //    count = count+1
+  //  }
+ // }return count;
+//}
+//console.log(countVowels('this is a mango and this is a star'));
+
+//const arrowCount = (sta) =>{
+	//let count = 0;
+	//for (let i of sta){
+	//	if(i=='a'||i=='e'||i=='o'||i=='u'){
+	//	 count++
+	//	}
+	//}return count;
+//}
+//console.log(arrowCount('hello'));*/ //
+
+// let arr = [1,2,3,4,5];
+// arr.forEach((val) => {
+//   console.log(val*val);
+// })
+
+// let nums = [94,99,98];
+// nums.map((num) =>{
+//   console.log(num)
+// });
+
+let arrs = [22,33,44];
+// let newArr = arrs.map((arr) => {
+//   return arr;
+// })
+// console.log(newArr);
+
+let newOrr = arrs.filter((val) => {
+  return val>40;
+});
+console.log(newOrr);
+
