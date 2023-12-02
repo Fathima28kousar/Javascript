f2ff3bacbb49680fe4bb9600f56d6a4a515dcf39
Fathima28kousar <kousar28fathima@gmail.com>
2023-12-02 22:10:17 +0530

    S

---
+++ Assignment-ProStack/2two.js
@@ -0,0 +1,119 @@
+var prompt = require('prompt-sync')();
+//21*******************************************
+// let num = prompt('Enter any number: ')
+// if (num%7==0){
+//     console.log(num,"is divisible by 7");
+// }else{
+//     console.log(num,"is not divisible by 7")
+// }
+
+//22*******************************************
+// let num1 = prompt('Enter any number: ')
+// if (num1%3==0){
+//     console.log(num1,"is a multiple of 3");
+// }else{
+//     console.log(num1,"is not a multiple of 3")
+// }
+
+//23*******************************************
+// let num = parseInt(prompt("Enter any number: "));
+// if (num>0){
+//     console.log(num,"is positive");
+// }else if(num<0){
+//     console.log(num,"is negative");
+// }else if (num==0){
+//     console.log("Given number is zero");
+// }else{
+//     console.log("please enter the number");
+// }
+
+//24*******************************************
+// let num = prompt("Enter any number: ");
+// if(num.length==3){
+//     console.log(`The given number ${num} is a 3-digit number`);
+// }else{
+//     console.log(`The given number ${num} is not a 3 digit number`);
+// }
+
+//25*******************************************
+//  let num = parseInt(prompt('Enter any number: '));
+// if (num%2 ==0){
+//     console.log('the given number is an even number');
+// }else{
+//     console.log('the given number is an odd number');
+// }
+// console.log(typeof num);
+
+//26*******************************************
+// let num1 = parseInt(prompt("Enter 1st number: "));
+// let num2 = parseInt(prompt("Enter 2nd number : "));
+// if (num1 > num2){
+//     console.log(num1,"is greater number");
+// }else{
+//     console.log(num2,'is greater number');
+// }
+
+//27*******************************************
+// let num1 = parseInt(prompt("Enter 1st number: "));
+// let num2 = parseInt(prompt("Enter 2nd number : "));
+// if (num1 < num2){
+//     console.log(num1,"is least number");
+// }else{
+//          console.log(num2,'is least number');
+// }
+
+//28*******************************************
+// let num1 = parseInt(prompt("Enter 1st number: "));
+// let num2 = parseInt(prompt("Enter 2nd number : "));
+// let num3 = parseInt(prompt("Enter 3rd number : "));
+
+// let greatest = Math.max(num1,num2,num3);
+// console.log(greatest,'is the greatest number');
+
+//29*******************************************
+// let num1 = parseInt(prompt("Enter 1st number: "));
+// let num2 = parseInt(prompt("Enter 2nd number : "));
+// let num3 = parseInt(prompt("Enter 3rd number : "));
+
+// let least = Math.min(num1,num2,num3);
+// console.log(least,'is the least number');
+
+//30*******************************************
+
+// let num1 = parseInt(prompt("Enter 1st number: "));
+// let num2 = parseInt(prompt("Enter 2nd number : "));
+// let num3 = parseInt(prompt("Enter 3rd number : "));
+// let numbers = [num1,num2,num3];
+// numbers.sort();
+// console.log(numbers);
+
+//31*******************************************
+// let num1 = parseInt(prompt("Enter 1st number: "));
+// let num2 = parseInt(prompt("Enter 2nd number : "));
+// let num3 = parseInt(prompt("Enter 3rd number : "));
+
+// let numbers = [num1,num2,num3];
+// numbers.reverse();
+// console.log(numbers);
+
+//32*******************************************
+// console.log(Math.max( 1,2,3));
+
+//33*******************************************
+// let num = prompt("Enter any number: ");
+// num%7 ==0 ? console.log(num,' is divisible by 7') : console.log(num," is not divisible by 7");
+
+//34*******************************************
+// let num = prompt("Enter any number: ");
+// num%3 ==0 ? console.log(num,' is multiple of 3') : console.log(num," is not multiple of 3");
+
+//35*******************************************
+// let num = prompt("Enter any number: ");
+// num>0 ? console.log(num,'is a positive number') : console.log(num,'is a negative number');
+
+//36*******************************************
+// let num = prompt("Enter any number : ");
+// num.length==3 ? console.log(num,'is a 3-digit number') : console.log(num,'is not a 3-digit number');
+
+// let num = 56;
+// num%2 ==0 ? console.log('even number') : console.log('odd number');
