ca7440496b85682926271f3b59ee2b561b195d01
Fathima28kousar <kousar28fathima@gmail.com>
2023-12-04 16:06:45 +0530

    E"

---
+++ Assignment-events/one.js
@@ -0,0 +1,11 @@
+function updatePage(){
+    // let cdate = new Date().toLocaleDateString();
+    // document.getElementById('abc').innerHTML = cdate
+    // document.getElementById('abc').innerHTML = 'good morning';
+    // document.write('GN');
+    // console.log('testCase 123');
+    // let ref = document.getElementById('abc');
+    // console.log(ref);
+    // ref.innerHTML = 'HEllo how are you??';
+}
+
