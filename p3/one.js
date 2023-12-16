f6c8e14b3784187b4a437625c34c1ed0d9b5a7a9
Fathima28kousar <kousar28fathima@gmail.com>
2023-12-16 14:02:03 +0530

    asynchronous js

--- p3/one.js
+++ p3/one.js
@@ -1,12 +1,34 @@
+// class Parent{
+//     assets = '100cr';
+// }
 
+// class Child extends Parent{
+//     qualities = 'good';
+// }
 
-function calculateResult() {
-    const currentDisplay = document.getElementById('display');
-    const inputValue = currentDisplay.value;
+// let c1 = new Child();
+// console.log(c1);
 
-    // Now you can use inputValue in your calculations or log it
-    console.log(inputValue);
+// let prices = [98,198,298,398,498]
+// let new_Prices=prices.forEach((price)=>{
+//     return price+1
+// })
 
-    // Example: Evaluate the expression and update the display
-    currentDisplay.value = eval(inputValue);
+// // for( price of prices){
+// //     new_Prices.push(price+1)
+// // }
+
+// console.log(prices)
+// console.log(new_Prices)
+
+function getData(dataId, getNextData){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('data', dataId);
+            resolve('success');
+            if(getNextData) {
+                getNextData();
+            }
+        },5000);
+    });
 }
