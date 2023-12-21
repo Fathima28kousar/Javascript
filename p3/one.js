29277728c51912f2dd9f6b10b520b35ef71b5189
Fathima28kousar <kousar28fathima@gmail.com>
2023-12-21 15:52:06 +0530

    modules

---
+++ p3/one.js
@@ -0,0 +1,65 @@
+// function getData(dataId){
+//     setTimeout(()=>{
+//         console.log('data',dataId);
+//     },2000);
+// }
+
+// getData(1);
+// getData(2);
+// getData(3);
+
+
+// function getData(dataId,getNextData){
+//     setTimeout(()=>{
+//         console.log('datas',dataId);
+//         if(getNextData){
+//             getNextData();
+//         }
+//     }, 2000);
+// }
+
+// console.log('fetching data1.............');
+// getData(1,()=>{
+//     console.log('fetching data2.............');
+//     getData(2,()=>{
+//     console.log('fetching data3.............');
+//         getData(3,()=>{
+//     console.log('fetching data4.............');
+//             getData(4,()=>{
+//                 console.log('completed!!!');
+//             })
+//         })
+//     })
+// });
+
+// function asyncFunc1() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log('data1');
+//             resolve('success!!!');
+//         }, 2000);
+//     });
+// }
+
+// function asyncFunc2() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log('data2');
+//             resolve(); // or reject('some error') if you want to simulate a rejection
+//         }, 2000);
+//     });
+// }
+
+// console.log('fetching data1........');
+// // let p1 = asyncFunc1();
+
+// // p1.then((res) => {
+// //     console.log('fetching data2..............');
+// //     let p2 = asyncFunc2();
+// //     p2.then((res)=>{});
+// // })
+// asyncFunc1().then((res)=>{
+//     console.log('fetching data2............');
+//     asyncFunc2().then((res)=>{});
+// })
+
