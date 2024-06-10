525a13d9fe1cfe422d36f9060e5443dee9f3161c
Fathima28kousar <kousar28fathima@gmail.com>
2024-06-10 21:25:38 +0530

    javascript practice 3

--- new-practice/one.js
+++ new-practice/one.js
@@ -1,36 +1,64 @@
-const person = {
-    name: 'fathima', 
-    age: 13, 
-    qulification: 'bsc', 
-    eyeColor: 'black',
-    fullName: function() {
-        return this.name + this.age;
-    },
-    languages: {
-        lang1: 'kannada',
-        lang2: 'english',
-        lang3: 'hindi',
-    },
-}
+// const person = {
+//     name: 'fathima', 
+//     age: 13, 
+//     qulification: 'bsc', 
+//     eyeColor: 'black',
+//     // fullName: function() {                            
+//     //     return this.name + this.age;
+//     // },
+//     // languages: {
+//     //     lang1: 'kannada',
+//     //     lang2: 'english',
+//     //     lang3: 'hindi',
+//     // },
+// }
 
-person.nationality = 'indian';
+// person.nationality = 'indian';
+// person.lastName = 'kousar';
+// console.log(person);
 
-console.log(person);
-person.study = function(){
-    return (this.qulification + this. age).toUpperCase();
-}
-console.log(person.study());
-console.log(person);
-console.log(person.languages.lang1);
-console.log(person['languages']['lang1']);
-console.log(person.languages['lang1'])
-let text = "";
-for (let x of person){
-    text += person[x] + " ";
-}
-document.getElementById('demo').innerHTML = text;
 
-// console.log(person.study());
-// const myArray = Object.values(person);
+// let para = Object.entries(person);
 
-// document.getElementById('demo').innerHTML = myArray;
+// console.log(para)
+
+// let text = "";
+// for (let [x,y] of Object.entries(person)){
+//     text += x + y;
+    
+// }
+// document.getElementById('demo').innerHTML = text;
+
+// // const myArray = Object.values(person)
+
+// // document.getElementById('demo').innerHTML = myArray;
+// // console.log(myArray)
+
+// // let text = "";
+// // for (let [key,value] of Object.entries(person)){
+// //     text += key + ": " + value + "<br>";
+// // }
+
+// // document.getElementById('demo2').innerHTML = text;
+// // console.log(text);
+
+
+
+// // let text="";
+// // for (let x in person){
+// //     text += person[x];
+// //     console.log(person[x])
+// // }
+
+let day;
+switch (new Date().getDay()) {
+    case 6:
+      text = "Today is Saturday";
+      break;
+    case 0:
+      text = "Today is Sunday";
+      break;
+    default:
+      text = "Looking forward to the Weekend";
+  }
+  console.log(text)
