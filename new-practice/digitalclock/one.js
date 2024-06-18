9f5ab20ab3f197855ca7ea5ecae1d5b0d7c531fb
Fathima28kousar <kousar28fathima@gmail.com>
2024-06-18 23:57:24 +0530

    tst

--- new-practice/digitalclock/one.js
+++ new-practice/digitalclock/one.js
@@ -1,28 +1,28 @@
-setInterval(showTime,1000);
+let slideBtnLeft = document.getElementById("slide-btn-left")
+let slideBtnRight = document.getElementById("slide-btn-right")
+let imgItem = document.querySelectorAll(".imageItem")
 
-function showTime(){
-    
-    let time = new Date();
-    let hour = time.getHours();
-    let min = time.getMinutes();
-    let sec = time.getSeconds();
-    am_pm = "AM";
+let startSlider = 0;
+let endSlider = (imgItem.length - 1) * 100
+slideBtnLeft.addEventListener('click',handleLeftBtn);
 
-    if(hour >= 12){
-        if(hour > 12){
-            hour = hour - 12;
-            am_pm = "PM";
-        }else if(hour == 0){
-            hour = 12;
-            am_pm = "AM";
-        }
+function handleLeftBtn(){
+    if(startSlider < 0){
+        startSlider = startSlider + 100;
     }
-
-    hour = hour < 10 ? "0" + hour : hour;
-    min = min < 10 ? "0" + min : min;
-    sec = sec < 10 ? "0" + sec : sec;
-
-    let currentTime = hour + ":" + min + ":" + sec + " " + am_pm;
-    document.getElementById('demo').innerHTML = currentTime;
+    console.log(startSlider);
+    imgItem.forEach(element => {
+        element.style.transform = `translateX(${startSlider}%)`
+    })
 }
 
+slideBtnRight.addEventListener("click",handleRightBtn)
+function handleRightBtn(){
+    if(startSlider >= -endSlider+100){
+        startSlider = startSlider - 100
+    }
+    console.log(startSlider)
+    imgItem.forEach(element => {
+        element.style.transform =`translateX(${startSlider}%)`
+    })
+}
